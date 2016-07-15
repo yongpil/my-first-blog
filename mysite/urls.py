d391868ca98c1abb3d1f6362bdbc168b1acb1362
myp7166 <myp7166@naver.com>
2016-07-15 22:40:49 +0900

    add secure function

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import django.contrib.auth.views
+admin.autodiscover()
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login' ),
+    url(r'^accounts/logout/$',django.contrib.auth.views.logout, name='logout', kwargs={'next_page':'/' }),
 ]
